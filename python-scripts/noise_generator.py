b830c953f10adee0df2d8593e05c5345440e012e
Gavin Hughes <49838674+RedstoneEngine@users.noreply.github.com>
2025-04-08 01:32:18 -0500

    Final Density

--- python-scripts/noise_generator.py
+++ python-scripts/noise_generator.py
@@ -6,12 +6,11 @@ import yaml
 # Function to convert Vanilla terms to Terra
 def noiseFunction(firstOctave, amplitude, index, size):
     frequency = 2**(firstOctave + index)
-    amplitude = 1.04 * 2 * amplitude * 2**(size - index - 1) / (2**size - 1)
+    amplitude = 1.15 * amplitude * 2**(size - index - 1) / (2**size - 1)
     return [frequency, amplitude]
 
 # Find Files
 noise_files = os.listdir('python-scripts/input/noise')
-print(noise_files)
 
 # Output Data
 data = {
@@ -37,15 +36,30 @@ for noise_file in noise_files:
     sampler["samplers"] = {}
     sampler["expression"] = "("
 
+    # Determine Multiplier
+    '''
+    minA = 100
+    maxA = -100
+    for i in range(len(amplitudes)):
+        if amplitudes[i] != 0:
+            minA = min(minA, i)
+            maxA = max(maxA, i)
+    mult = 5 / (3 * (1 + 1 / (maxA - minA + 1)))
+    '''
+    mult = 1
+
     # Sampler Combination
     for i in range(len(amplitudes)):
-        perlinParams = noiseFunction(firstOctave, amplitudes[i], i, len(amplitudes))
-        sampler["samplers"]["noise" + str(i)] = {"type": "PERLIN", "dimensions": dim}
-        sampler["samplers"]["noise" + str(i)]["frequency"] = perlinParams[0]
-        if dim == 2:
-            sampler["expression"] += str(perlinParams[1]) + " * noise" + str(i) + "(x, z) + "
-        else:
-            sampler["expression"] += str(perlinParams[1]) + " * noise" + str(i) + "(x, y, z) + "
+        if amplitudes[i] > 0:
+            perlinParams = noiseFunction(firstOctave, amplitudes[i], i, len(amplitudes))
+            #sampler["samplers"]["noise" + str(i)] = {"type": "NORMAL", "mean": 0, "standard-deviation": .325, "dimensions": dim,
+            #    "sampler": {"type": "PERLIN", "dimensions": dim}}
+            sampler["samplers"]["noise" + str(i)] = {"type": "OPEN_SIMPLEX_2", "dimensions": dim}
+            sampler["samplers"]["noise" + str(i)]["frequency"] = perlinParams[0]
+            if dim == 2:
+                sampler["expression"] += str(perlinParams[1] * mult) + " * noise" + str(i) + "(x, z) + "
+            else:
+                sampler["expression"] += str(perlinParams[1] * mult) + " * noise" + str(i) + "(x, y, z) + "
     
     # Expression Cleanup
     sampler["expression"] = sampler["expression"][:-3] + ")"
